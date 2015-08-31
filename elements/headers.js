b41326d9b69cf1ce2989f7b5299d698aeb8ba8c6
sethvincent <sethvincent@gmail.com>
2015-08-31 16:52:27 -0700

    rename columns

--- elements/headers.js
+++ elements/headers.js
@@ -1,5 +1,6 @@
 var element = require('base-element')
 var inherits = require('inherits')
+var createPopup = require('./popup')
 
 module.exports = Headers
 inherits(Headers, element)
@@ -19,9 +20,60 @@ Headers.prototype.render = function (properties) {
       property.name,
       self.html('button#column-settings.small', {
         onclick: function (e) {
-          self.send('destroy-column', property, e)
+          var popup = createPopup(function () {
+            self.send('close', property)
+          })
+
+          popup.addEventListener('render', function (el) {
+            self.send('render', el, property)
+          })
+
+          popup.addEventListener('close', function (el) {
+            self.send('close', el, property)
+          })
+
+          var newName = ''
+          var el = popup.open([
+            self.html('h1', property.name + ' settings'),
+            self.html('h2', 'Manage the settings of this column'),
+            self.html('h3', 'Change column type'),
+            self.html('input.small.change-column-type', {
+              type: 'text',
+              oninput: function (e) {
+                
+              }
+            }),
+            self.html('button.small.button-blue', {
+              onclick: function (e) {
+                
+              }
+            }, 'Change type'),
+            self.html('hr'),
+            self.html('h3', 'Rename column'),
+            self.html('input.small.rename-column', {
+              type: 'text',
+              oninput: function (e) {
+                newName = e.target.value
+                
+              }
+            }),
+            self.html('button.small.button-blue', {
+              onclick: function (e) {
+                self.send('rename-column', property, newName, e)
+              }
+            }, 'Rename'),
+            self.html('hr'),
+            self.html('h3', 'Destroy column'),
+            self.html('button.small.button-red', {
+              onclick: function (e) {
+                self.send('destroy-column', property, e)
+              }
+            }, 'Destroy ' + property.name + ' column')
+          ])
+
+          self.send('render', el, properties[key])
         }
-      }, self.html('i.fa.fa-trash', ''))
+      }, self.html('i.fa.fa-gear', ''))
     ]))
   })
 
