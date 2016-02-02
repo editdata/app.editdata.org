b052fbb8f75f1696071a97163641341d10fb7741
Kevin Neff <kvnneff@gmail.com>
2016-02-02 10:11:04 -0800

    List all necessary props at top of function.

--- elements/menu-bar.js
+++ elements/menu-bar.js
@@ -2,41 +2,44 @@ var h = require('virtual-dom/h')
 var Menu = require('menu-element')()
 var MenuItem = require('menu-element/item')
 var Dropdown = require('menu-element/dropdown')
-var actions = require('../actions')
 
 module.exports = MenuBar
 
+/**
+ * Editor Menu Bar
+ * @param {Object} props
+ */
 function MenuBar (props) {
-  var openFileDropdownIsVisible = props.menus.openFile
-  var store = props.store
+  var actions = props.actions || {}
+  var menus = props.menus || {}
+
+  var openNew = actions.openNew
+  var newRow = actions.newRow
+  var modal = actions.modal
+  var menu = actions.menu
 
   function openNewFilePopup (event) {
     var slug = event.target.id
-    actions.editor.openNew(slug, store)
+    openNew(slug)
   }
 
   function showSave () {
-    actions.modal('saveNewFile', true, store)
+    modal('saveNewFile', true)
   }
 
-  var openEmpty = MenuItem({
-    id: 'empty',
-    text: 'New empty dataset',
-    onclick: openNewFilePopup
-  })
+  function createNewRow () {
+    newRow()
+  }
 
-  var openGithub = MenuItem({
-    id: 'github',
-    text: 'GitHub',
-    onclick: openNewFilePopup
-  })
+  function newColumn () {
+    modal('createNewColumn', true)
+  }
+
+  var openEmpty = MenuItem({ id: 'empty', text: 'New empty dataset', onclick: openNewFilePopup })
+  var openGithub = MenuItem({ id: 'github', text: 'GitHub', onclick: openNewFilePopup })
 
   // var openDat = MenuItem({ id: 'dat', text: 'Dat' })
-  var openUpload = MenuItem({
-    id: 'upload',
-    text: 'Upload CSV or JSON',
-    onclick: openNewFilePopup
-  })
+  var openUpload = MenuItem({ id: 'upload', text: 'Upload CSV or JSON', onclick: openNewFilePopup })
 
   var openDropdown = Dropdown({
     id: 'open',
@@ -47,16 +50,15 @@ function MenuBar (props) {
       openUpload
     ],
     onclick: function () {
-      actions.menu('openFile', true, store)
+      menu('openFile', true)
 
-      // TODO: Find a better way to do this
-      var listener = function (e) {
+      function listener (e) {
         var found = false
         for (var element = e.target; element; element = element.parentNode) {
           if (element.id === 'open') found = true
         }
         if (!found) {
-          actions.menu('openFile', false, store)
+          if (menu) menu('openFile', false)
           document.body.removeEventListener('click', listener)
         }
       }
@@ -67,26 +69,12 @@ function MenuBar (props) {
 
   var save = MenuItem({ id: 'save', text: 'Save', onclick: showSave })
   var exportItem = MenuItem({ id: 'export', text: 'Export', onclick: showSave })
-
-  var newRowMenu = MenuItem({
-    id: 'new-row',
-    text: 'New row',
-    onclick: function () {
-      actions.editor.newRow(store)
-    }
-  })
-
-  var newColumnMenu = MenuItem({
-    id: 'new-column',
-    text: 'New column',
-    onclick: function () {
-      actions.modal('createNewColumn', true, store)
-    }
-  })
+  var newRowMenu = MenuItem({ id: 'new-row', text: 'New row', onclick: createNewRow })
+  var newColumnMenu = MenuItem({ id: 'new-column', text: 'New column', onclick: newColumn })
 
   return h('div#menu', [
     Menu.render([
-      openDropdown.render({ open: openFileDropdownIsVisible }),
+      openDropdown.render({ open: menus.openFile }),
       save.render(props),
       exportItem.render(props),
       newColumnMenu.render(props),
