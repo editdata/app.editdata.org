9d35a0cfe2f0945393f6b6d9c79809714722f92f
Kevin Neff <kvnneff@gmail.com>
2016-02-04 11:49:05 -0800

    Add box styling

--- elements/about.js
+++ elements/about.js
@@ -3,7 +3,7 @@ var h = require('virtual-dom').h
 module.exports = About
 
 function About () {
-  return h('div.about', [
+  return h('div.about.content-box', [
     h('h1', 'About EditData.org'),
     h('h2', 'Hello. Let me tell you about everything.'),
     h('p', 'EditData.org is a tool for editing CSV & JSON files from your computer & from GitHub.'),
