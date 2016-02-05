6cae84da61b076caad15e47b68b1ed513b30709a
Kevin Neff <kvnneff@gmail.com>
2016-02-05 14:55:11 -0800

    Create notification element.

--- elements/notify.js
+++ elements/notify.js
@@ -1,47 +1,18 @@
-var BaseElement = require('base-element')
-var inherits = require('inherits')
+var h = require('virtual-dom/h')
 
-module.exports = Notify
-inherits(Notify, BaseElement)
+module.exports = Notification
 
-function Notify (options, onclose) {
-  if (!(this instanceof Notify)) return new Notify(options, onclose)
-  BaseElement.call(this)
-  if (typeof options === 'function') {
-    onclose = options
-    options = {}
-  }
-  this.onclose = onclose
-  this.showing = false
-}
+function Notification (props) {
+  var actions = props.actions
+  var message = props.message
+  var type = props.type
+  var close = actions.close
 
-Notify.prototype.render = function (state) {
-  var h = this.html
-  var self = this
-  var vtree
-
-  if (this.showing) {
-    vtree = h('div.notify.notify-' + state.type, {
-      onclick: function (e) {
-        self.hide()
-      }
-    }, [
-      h('div.message', state.message)
-    ])
-  } else {
-    vtree = h('div.notify.hidden')
-  }
-
-  return this.afterRender(vtree)
-}
-
-Notify.prototype.show = function (options) {
-  this.showing = true
-  return this.render(options)
-}
-
-Notify.prototype.hide = function () {
-  this.showing = false
-  if (this.onclose) this.onclose()
-  return this.render({})
+  return h('div.notify.notify-' + type, {
+    onclick: function (e) {
+      close(e)
+    }
+  }, [
+    h('div.message', message)
+  ])
 }
